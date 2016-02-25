24502555e794dab8983733c67ff22552123d6d3f
anzileiro <anderson.anzileiro@gmail.com>
2016-02-25 12:38:43 -0300

    provision teste

---
+++ mongodb/provision_mongodb.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#Script para automatizar vagrant boxe. Instalar o MongoDB 3.2.3
+#alterar aqui
+
+install_mongodb(){
+	echo "Instalando o mongodb..." && \
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 && \
+	echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list && \
+	sudo apt-get update && \
+	sudo apt-get install -y mongodb-org && \
+	sudo apt-get install -y mongodb-org=3.2.3 mongodb-org-server=3.2.3 mongodb-org-shell=3.2.3 mongodb-org-mongos=3.2.3 mongodb-org-tools=3.2.3 && \
+	echo "mongodb-org hold" | sudo dpkg --set-selections && \
+	echo "mongodb-org-server hold" | sudo dpkg --set-selections && \
+	echo "mongodb-org-shell hold" | sudo dpkg --set-selections && \
+	echo "mongodb-org-mongos hold" | sudo dpkg --set-selections && \
+	echo "mongodb-org-tools hold" | sudo dpkg --set-selections && \
+
+	sudo service mongod start
+
+	sudo sed "s/bindIp: 127.0.0.1/#bindIp: 127.0.0.1" /etc/mongod.conf
+
+	sudo service mongod restart
+}
+
+echo ">>> Instalando o MongoDB 3.2.3"
+install_mongodb
