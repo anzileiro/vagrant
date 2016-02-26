5f72329e4d90ebf760b306668e57d24af0834f3d
anzileiro <anderson.anzileiro@gmail.com>
2016-02-26 20:40:10 -0300

    change for redis provision

--- redis/provision_redis.sh
+++ redis/provision_redis.sh
@@ -5,15 +5,8 @@ install_redis(){
 	sudo wget http://download.redis.io/redis-stable.tar.gz && \
 	sudo tar xvzf redis-stable.tar.gz && \
 	cd redis-stable && \
-	sudo make install
-
-	sudo cp /vagrant/redis.upstart.conf /etc/init/redis.conf && \
-	sudo cp /vagrant/redis.conf /etc/redis.conf && \
-	sudo service redis start && \
-
-	sudo cp /vagrant/redis-sentinel.upstart.conf /etc/init/redis-sentinel.conf && \
-	sudo cp /vagrant/redis-sentinel.conf /etc/redis-sentinel.conf && \
-	sudo service redis-sentinel start
+	sudo make install && \
+	sudo service redis start
 }
 
 echo ">>> Instalando o Redis"
